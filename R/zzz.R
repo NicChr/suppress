89917c3b235d8b7d4cc19147e3ec0ad42c6d249a
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2023-11-06 16:45:03 +0000

    Updates.

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,8 @@
 .onLoad <- function(...) {
   S7::methods_register()
 }
+.onAttach <- function(...) {
+  options(suppress.threshold = getOption("suppress.threshold", 5),
+          suppress.replace = getOption("suppress.replace", NA),
+          suppress.digits = getOption("suppress.digits", 2))
+}
